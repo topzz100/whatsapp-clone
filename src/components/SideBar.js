64fe15a3b6564c922af67878eb5986e7baff5651
topzz100 <adenekantope@gmail.com>
2021-12-14 17:21:20 +0100

    side-bar header and search-bar done

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Avatar, IconButton} from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert'; 
+import ChatIcon from '@mui/icons-material/Chat'; 
+import DonutLargeIcon from '@mui/icons-material/DonutLarge'; 
+
+import './SideBar.css';
+import { SearchOutlined } from '@mui/icons-material';
 
 const SideBar = () => {
   return (
-    <div>
-      sidebar
+    <div className = "sideBar">
+      <div className = "sideBarHeader">
+        <Avatar/>
+        <div className = "sideBarHeaderRight">
+          <IconButton>
+            <DonutLargeIcon/>
+          </IconButton>
+           <IconButton>
+            <ChatIcon/>
+          </IconButton>
+           <IconButton>
+            <MoreVertIcon/>
+          </IconButton>
+ 
+        </div>
+      </div>
+      <div className = "sideBarSearch">
+        <div className ="sideBarSearchContainer">
+          <SearchOutlined/>
+          <input placeholder='Search' type = "text"/>
+        </div>
+          
+      </div>
+      <div className = "sideBarChats">
+          
+      </div>
+      <div></div>
     </div>
   )
 }
